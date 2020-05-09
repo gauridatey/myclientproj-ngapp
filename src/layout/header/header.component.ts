39a168d8e6e81fa998b238f61070217f98a540d7
Manisha <manisha_msathe@yahoo.com>
2020-05-09 15:55:15 +0530

    change is selected lang

--- src/layout/header/header.component.ts
+++ src/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import { environment } from './../../environments/environment';
-import { PlatformLocation } from '@angular/common';
 import { ConstantService } from './../../app/shared/constant.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 interface Language {
-  code: string;
+  language: string;
+  baseHref: string;
   label: string;
   url: string;
 }
@@ -13,27 +14,26 @@ interface Language {
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
-  selectedLangUrl: string;
-  selectedLang: string; // coming from basehref
   languageList: Language[] = [];
+  selectedLang: Language;
 
-  constructor(private constantService: ConstantService, private platformLocation: PlatformLocation) {
-    this.selectedLang = (this.platformLocation as any).location.pathname.substring(1, 3);
+  constructor(@Inject(APP_BASE_HREF) private baseHref: string, private constantService: ConstantService) {
     this.languageList = [
-      { code: 'en', label: 'English', url: environment.LANG_EN_URL },
-      { code: 'fr', label: 'French', url: environment.LANG_FR_URL },
-      { code: 'mr', label: 'Marathi', url: environment.LANG_MR_URL }
+      { language: 'en', baseHref: '/', label: 'English', url: environment.LANG_EN_URL },
+      { language: 'fr', baseHref: '/fr', label: 'French', url: environment.LANG_FR_URL },
+      { language: 'mr', baseHref: '/mr', label: 'Marathi', url: environment.LANG_MR_URL }
     ];
-    if (typeof this.selectedLang === 'undefined' || this.selectedLang === null || this.selectedLang === '') {
-      this.selectedLang = this.languageList[0].code;
-      this.selectedLangUrl = this.languageList[0].url;
+    // get default selectedLang depending on baseHref
+    if (!this.languageList.some(item => item.baseHref === this.baseHref)) {
+      this.selectedLang = this.languageList[0];
     } else {
-      this.selectedLangUrl = this.languageList.filter(item => item.code === this.selectedLang)[0].url;
+      this.selectedLang = this.languageList.filter(item => item.baseHref === this.baseHref)[0];
     }
   }
 
-  onChangeLang() {
-    this.constantService.redirectToExternalUrl(this.selectedLangUrl);
+  onChangeLang(language: Language) {
+    this.constantService.redirectToExternalUrl(language.url);
   }
+
   ngOnInit() {}
 }
